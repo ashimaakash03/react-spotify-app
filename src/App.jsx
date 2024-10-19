c539fa5c43a061affc32cc75607ea3ea5082b711
713068_Ashim <ashimaakash1997@gmail.com>
2024-10-19 12:05:10 +0530

    completed building sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 //import React from "react";
 
+import Sidebar from "./components/Sidebar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex bg-white">
+        <Sidebar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
