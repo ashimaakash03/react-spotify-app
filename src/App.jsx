b657e98dfd442a699d6202cf6af9486ff00dccfe
713068_Ashim <ashimaakash1997@gmail.com>
2024-10-19 12:09:41 +0530

    created and mounted Player component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 //import React from "react";
 
+import Player from "./components/Player";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className="h-screen bg-black">
       <div className="h-[90%] flex bg-white">
         <Sidebar />
+        <Player />
       </div>
     </div>
   );
