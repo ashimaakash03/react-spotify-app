ecc7be84b5d7dc1a96cd0b3800973d111d432d7c
713068_Ashim <ashimaakash1997@gmail.com>
2024-10-20 15:59:35 +0530

    added route to album details page

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,9 +1,14 @@
 //import React from 'react'
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-const AlbumItem = ({ image, name, desc }) => {
+const AlbumItem = ({ image, name, desc, id }) => {
+  const navigate = useNavigate();
   return (
-    <div className="min-w-[100px] rounded p-3 cursor-pointer hover:bg-[#ffffff26]">
+    <div
+      onClick={() => navigate(`/album/${id}`)}
+      className="min-w-[100px] rounded p-3 cursor-pointer hover:bg-[#ffffff26]"
+    >
       <img className="rounded" src={image} />
       <p className="font-bold mt-2 mb-1">{name}</p>
       <p className="text-slate-200 text-sm">{desc.slice(0, 19)}</p>
